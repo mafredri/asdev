0411d47ce3701183f5f75b4566ee99498b465485
Mathias Fredriksson <mafredri@gmail.com>
2017-08-07 03:20:49 +0300

    Refactor

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"html"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -18,8 +16,6 @@ import (
 
 	"github.com/mafredri/cdp"
 	"github.com/mafredri/cdp/devtool"
-	"github.com/mafredri/cdp/protocol/dom"
-	"github.com/mafredri/cdp/protocol/runtime"
 	"github.com/mafredri/cdp/rpcc"
 	"github.com/pkg/errors"
 )
@@ -160,43 +156,8 @@ func run(ctx context.Context, verbose, headless bool, chromeBin string, username
 	return nil
 }
 
-func upload(ctx context.Context, verbose bool, devt *devtool.DevTools, errc chan<- error, apps []App, apk *apkg.File) (err error) {
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithCancel(ctx)
-	defer cancel()
-
-	defer func() {
-		select {
-		case <-ctx.Done():
-			err = ctx.Err()
-		default:
-		}
-		errc <- err
-	}()
-
-	tab, err := devt.Create(ctx)
-	if err != nil {
-		return err
-	}
-
-	conf, err := apk.Config()
-	if err != nil {
-		return errors.Wrap(err, "could read apk config")
-	}
-
-	var opts []rpcc.DialOption
-	if verbose {
-		opts = append(opts, newLogCodec(conf.General.Package+":"+conf.General.Architecture))
-	}
-	conn, err := rpcc.DialContext(ctx, tab.WebSocketDebuggerURL, opts...)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	c := cdp.NewClient(conn)
-
-	targetCrashed, err := c.Inspector.TargetCrashed(ctx)
+func abortOnDetachOrCrash(ctx context.Context, ic cdp.Inspector, abort func(err error)) error {
+	targetCrashed, err := ic.TargetCrashed(ctx)
 	if err != nil {
 		return nil
 	}
@@ -211,11 +172,10 @@ func upload(ctx context.Context, verbose bool, devt *devtool.DevTools, errc chan
 			return
 		}
 		log.Println("Target crashed!")
-		errc <- errors.New("target crashed")
-		cancel()
+		abort(errors.New("target crashed"))
 	}()
 
-	detached, err := c.Inspector.Detached(ctx)
+	detached, err := ic.Detached(ctx)
 	if err != nil {
 		return nil
 	}
@@ -230,143 +190,8 @@ func upload(ctx context.Context, verbose bool, devt *devtool.DevTools, errc chan
 			return
 		}
 		log.Printf("Inspector detached: %v!", ev.Reason)
-		errc <- fmt.Errorf("inspector detached: %v", ev.Reason)
-		cancel()
+		abort(fmt.Errorf("inspector detached: %v", ev.Reason))
 	}()
-
-	app := appSlice(apps).Find(conf.General.Package, conf.General.Architecture)
-
-	absPath, err := filepath.Abs(apk.Path())
-	if err != nil {
-		return err
-	}
-
-	for _, fn := range []func() error{
-		func() error { return c.Inspector.Enable(ctx) },
-		func() error { return c.DOM.Enable(ctx) },
-		func() error { return c.Runtime.Enable(ctx) },
-		func() error { return navigate(ctx, c.Page, app.UpdateURL(), 10*time.Second) },
-		func() error { return setFormInputFiles(ctx, c, "#appFile", absPath) },
-		func() error { return submitForm(ctx, c, `document.getElementById('mainform').submit()`) },
-	} {
-		if err = fn(); err != nil {
-			return err
-		}
-	}
-
-	// Fetch the document root for querying elements.
-	doc, err := c.DOM.GetDocument(ctx, nil)
-	if err != nil {
-		return err
-	}
-
-	// Fetch all product category options so that we can inherit them from
-	// the previous version that was published.
-	catOpts, err := c.DOM.QuerySelectorAll(ctx, &dom.QuerySelectorAllArgs{
-		NodeID:   doc.Root.NodeID,
-		Selector: "#category_id option",
-	})
-	if err != nil {
-		return err
-	}
-
-	for _, o := range catOpts.NodeIDs {
-		res, err := c.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{NodeID: o})
-		if err != nil {
-			return err
-		}
-
-		// TODO: Cleanup this limitation of the Chrome Debuggin Protocol.
-		// It only gives us OuterHTML.
-		text := html.UnescapeString(optionTagRe.ReplaceAllString(res.OuterHTML, ""))
-
-		if app.HasCategory(text) {
-			err = c.DOM.SetAttributeValue(ctx, &dom.SetAttributeValueArgs{
-				NodeID: o,
-				Name:   "selected",
-				Value:  "selected",
-			})
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	descr := apk.Description()
-	defer descr.Close()
-	descb, err := ioutil.ReadAll(descr)
-	if err != nil {
-		return err
-	}
-
-	chlogr := apk.Changelog()
-	defer chlogr.Close()
-	chlogb, err := ioutil.ReadAll(chlogr)
-	if err != nil {
-		return err
-	}
-
-	for _, set := range []struct {
-		id       string
-		value    string
-		textarea bool
-	}{
-		{id: "#name_en_US", value: conf.General.Name, textarea: false},
-		// {id: "#tags_en_US", value: "", textarea: false},
-		{id: "#description_en_US", value: string(descb), textarea: true},
-		{id: "#changes_en_US", value: string(chlogb), textarea: true},
-	} {
-		if set.textarea {
-			// TODO: Figure out how to set text content of textarea without eval.
-			c.Runtime.Evaluate(ctx, &runtime.EvaluateArgs{
-				Expression: fmt.Sprintf(`document.querySelector(%q).textContent = %q`, set.id, set.value),
-			})
-			continue
-		}
-
-		sel, err := c.DOM.QuerySelector(ctx, &dom.QuerySelectorArgs{
-			NodeID:   doc.Root.NodeID,
-			Selector: set.id,
-		})
-		if err != nil {
-			return err
-		}
-
-		err = c.DOM.SetAttributeValue(ctx, &dom.SetAttributeValueArgs{
-			NodeID: sel.NodeID,
-			Name:   "value",
-			Value:  set.value,
-		})
-		if err != nil {
-			return err
-		}
-		continue
-	}
-
-	// Inherit the beta status for this app.
-	if app.Beta {
-		// Beta status is decided by #radio (Yes) and #radio2 (No).
-		sel, err := c.DOM.QuerySelector(ctx, &dom.QuerySelectorArgs{
-			NodeID:   doc.Root.NodeID,
-			Selector: "#radio",
-		})
-		if err != nil {
-			return err
-		}
-		err = c.DOM.SetAttributeValue(ctx, &dom.SetAttributeValueArgs{
-			NodeID: sel.NodeID,
-			Name:   "checked",
-			Value:  "checked",
-		})
-		if err != nil {
-			return err
-		}
-	}
-
-	err = submitForm(ctx, c, `document.getElementById('mainform').submit()`)
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
